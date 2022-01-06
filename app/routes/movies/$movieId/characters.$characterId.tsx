1073e3ae76ec4eb78c30709d0e953758445ce827
KalsonSaint <saintkalson@gmail.com>
2022-01-06 16:46:31 +0100

    characterList updated and prefetch implemented

--- app/routes/movies/$movieId/characters.$characterId.tsx
+++ app/routes/movies/$movieId/characters.$characterId.tsx
@@ -16,7 +16,7 @@ const Character = () => {
 
   return (
     <div className="mb-3">
-      <div className="text-3xl mb-2">Character Details</div>
+      <div className="text-xl mb-2">Character Details</div>
       <div className="p-4 rounded shadow-lg border">
         <div className="text-gray-700 font-bold text-xl mb-2">{name}</div>
         <ul className="py-2">
