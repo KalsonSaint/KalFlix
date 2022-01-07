6f8e9e415519483a30f79362fef4cd245c324471
KalsonSaint <saintkalson@gmail.com>
2022-01-07 12:44:09 +0100

    error handling

--- app/root.tsx
+++ app/root.tsx
@@ -52,22 +52,29 @@ export const CatchBoundary = () => {
 
   return (
     <Document title={`${caught.status} ${caught.statusText}`}>
-      <div className="error-container">
-        <h1>
-          {caught.status} {caught.statusText}
-        </h1>
+      <div className="mb-3">
+        <div className="text-3xl mb-2">Details</div>
+        <div className="p-4 rounded shadow-lg border bg-orange-200 border-orange-600">
+          <div className="text-gray-700 font-bold text-xl mb-2">
+            {caught.statusText}
+          </div>
+          <p>
+            {caught.status} {caught.statusText}
+          </p>
+        </div>
       </div>
     </Document>
   );
 };
 
-export const ErrorBoundary = ({ error }: { error: Error }) => {
-  console.log(error);
-
+export const ErrorBoundary = ({ error }: any) => {
   return (
     <Document title="Error!!!">
-      <div className="error-container">
-        <h1>Something went wrong</h1>
+      <div className="text-3xl mb-2">Details</div>
+      <div className="p-4 rounded shadow-lg border bg-rose-200 border-rose-600">
+        <h1 className="text-gray-700 font-bold text-xl mb-2">
+          Something went wrong
+        </h1>
         <p>{error.message}</p>
       </div>
     </Document>
--- app/routes/movies/$movieId/characters.$characterId.tsx
+++ app/routes/movies/$movieId/characters.$characterId.tsx
@@ -1,4 +1,4 @@
-import { LoaderFunction, useLoaderData } from "remix";
+import { LoaderFunction, useCatch, useLoaderData } from "remix";
 import invariant from "tiny-invariant";
 import { getMovieCharacter } from "~/api/movies";
 import { MovieCharacter } from "~/types";
@@ -31,3 +31,35 @@ const Character = () => {
 };
 
 export default Character;
+
+// export const CatchBoundary = () => {
+//   let caught = useCatch();
+
+//   return (
+//     <div className="mb-3">
+//       <div className="text-3xl mb-2">Details</div>
+//       <div className="p-4 rounded shadow-lg border bg-orange-200 border-orange-600">
+//         <div className="text-gray-700 font-bold text-xl mb-2">
+//           {caught.statusText}
+//         </div>
+//         <p>
+//           {caught.status} {caught.statusText}
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export const ErrorBoundary = ({ error }: any) => {
+//   return (
+//     <div className="mb-3">
+//       <div className="text-3xl mb-2">Details</div>
+//       <div className="p-4 rounded shadow-lg border bg-rose-200 border-rose-600">
+//         <h1 className="text-gray-700 font-bold text-xl mb-2">
+//           Something went wrong
+//         </h1>
+//         <p>{error.message}</p>
+//       </div>
+//     </div>
+//   );
+// };
