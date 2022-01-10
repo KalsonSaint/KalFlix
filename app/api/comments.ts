5bda73604e8deb43a261810f7d089bce25329417
KalsonSaint <saintkalson@gmail.com>
2022-01-10 18:03:17 +0100

    addComment API

--- app/api/comments.ts
+++ app/api/comments.ts
@@ -1,7 +1,21 @@
+import { CommentEntry } from "./../types/CommentEntry.types";
+
 export const getComments = async (movieId: string) => {
   const response = await fetch(
     `http://localhost:3001/comments?movieId=${movieId}`
   );
+
+  return response.json();
+};
+
+export const addComment = async (comment: CommentEntry) => {
+  const response = await fetch(`http://localhost:3001/comments`, {
+    method: "POST",
+    body: JSON.stringify(comment),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
   return response.json();
 };
